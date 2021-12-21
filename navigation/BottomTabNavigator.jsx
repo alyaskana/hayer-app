3e256dc1fb88c85f43ff3781fe451848baa15899
Alina Bychkova <ialina240200@gmail.com>
2021-12-21 23:26:42 +0300

    update navbar

--- navigation/BottomTabNavigator.jsx
+++ navigation/BottomTabNavigator.jsx
@@ -3,20 +3,67 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import { Text, View } from "react-native";
 import { FeedScreen, ProfileScreen, PostScreen } from "../screens";
 
 const BottomTab = createBottomTabNavigator();
 
 export const BottomTabNavigator = () => {
   return (
-    <BottomTab.Navigator>
+    <BottomTab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          height: 120,
+
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        },
+      }}
+    >
       <BottomTab.Screen
         name="Лента"
         component={FeedNavigator}
         options={{
           headerShown: false,
-          tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
+          tabBarLabelStyle: {
+            fontSize: 16,
+            marginBottom: 12,
+            fontWeight: "500",
+          },
+          tabBarIcon: () => false,
+          tabBarIconStyle: { display: "none" },
+        }}
+      />
+
+      <BottomTab.Screen
+        name="add"
+        component={ProfileNavigator}
+        options={{
+          tabBarButton: () => (
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                height: 48,
+                paddingLeft: 36,
+                paddingRight: 36,
+                borderRadius: 100,
+                backgroundColor: "#1C63EC",
+              }}
+            >
+              <Text
+                style={{
+                  color: "#ffffff",
+                  lineHeight: 32,
+                  fontSize: 28,
+                }}
+              >
+                +
+              </Text>
+            </View>
+          ),
         }}
       />
       <BottomTab.Screen
@@ -24,9 +71,13 @@ export const BottomTabNavigator = () => {
         component={ProfileNavigator}
         options={{
           headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="ios-code" color={color} />
-          ),
+          tabBarLabelStyle: {
+            fontSize: 16,
+            marginBottom: 12,
+            fontWeight: "500",
+          },
+          tabBarIcon: () => false,
+          tabBarIconStyle: { display: "none" },
         }}
       />
     </BottomTab.Navigator>
