2c251aa08e1414ff540de17765b759a83cf21fcb
Alina Bychkova <ialina240200@gmail.com>
2022-05-15 14:42:29 +0300

    Add auth, login screens

---
+++ screens/auth/AuthScreen.tsx
@@ -0,0 +1,64 @@
+import { Colors } from "constants/Colors";
+import { FC } from "react";
+import {
+  Pressable,
+  SafeAreaView,
+  Dimensions,
+  StyleSheet,
+  View,
+} from "react-native";
+import { Button } from "shared/components";
+import { Caption_1 } from "shared/ui/Typography";
+
+export const AuthScreen: FC<{ navigation: any }> = ({ navigation }) => {
+  return (
+    <SafeAreaView>
+      <View style={s.wrap}>
+        <View style={s.buttons}>
+          <Pressable onPress={() => navigation.navigate("Login")}>
+            <Caption_1 style={s.loginText}>
+              Уже зарегистрированы?{" "}
+              <Caption_1 style={s.loginButton}>Войти</Caption_1>
+            </Caption_1>
+          </Pressable>
+          <Button
+            style={s.signUp}
+            text="Зарегистрироваться"
+            variant="primary"
+            onPress={() => navigation.navigate("SignUp")}
+          />
+          <Button variant="secondary" text="Пропустить" style={s.skipBtn} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const s = StyleSheet.create({
+  wrap: {
+    paddingHorizontal: 8,
+    display: "flex",
+    alignItems: "center",
+    position: "relative",
+    height: Dimensions.get("window").height - 47 - 34 - 44,
+  },
+  buttons: {
+    width: "100%",
+    position: "absolute",
+    bottom: 0,
+  },
+  loginText: {
+    color: Colors.Main.Gray_2,
+    textAlign: "center",
+  },
+  loginButton: {
+    color: Colors.Main.Primary,
+  },
+  signUp: {
+    marginTop: 24,
+    width: "100%",
+  },
+  skipBtn: {
+    marginTop: 8,
+  },
+});
---
+++ screens/auth/LoginScreen.tsx
@@ -0,0 +1,98 @@
+import { FC } from "react";
+import { SafeAreaView, View, Text } from "react-native";
+
+import { Button } from "shared/components";
+import { FieldSet, Form, Input, Label } from "shared/components/Form";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { useAuth } from "shared/hooks/useAuth";
+
+type FormInputs = {
+  email: string;
+  password: string;
+};
+
+export const LoginScreen: FC<{ navigation: any }> = ({ navigation }) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormInputs>({
+    mode: "onBlur",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const { token, login } = useAuth();
+  console.log(token);
+
+  const onSubmit: SubmitHandler<FormInputs> = ({ email, password }) => {
+    login(email, password).then(() => {
+      navigation.navigate("Home", {}, { screen: "Feed" });
+    });
+  };
+
+  return (
+    <SafeAreaView>
+      <View style={{ paddingHorizontal: 8, paddingTop: 5 }}>
+        <Form>
+          <FieldSet>
+            <Label title="Почта" />
+            <Controller
+              control={control}
+              name="email"
+              rules={{
+                required: "Это обязательное поле",
+              }}
+              render={({ field: { onChange, value, onBlur } }) => (
+                <Input
+                  textContentType="emailAddress"
+                  placeholder="Enter your email here"
+                  value={value}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  error={errors?.email?.message}
+                />
+              )}
+            />
+          </FieldSet>
+          <Label title="Пароль" />
+          <Controller
+            control={control}
+            name="password"
+            rules={{
+              required: "Пароль должен содержать минимум 6 символов",
+              minLength: {
+                value: 6,
+                message: "Пароль должен содержать минимум 6 символов",
+              },
+            }}
+            render={({ field: { onChange, value, onBlur } }) => (
+              <Input
+                placeholder="••••••"
+                secureTextEntry
+                textContentType="password"
+                value={value}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                error={errors?.password?.message}
+              />
+            )}
+          />
+        </Form>
+        <Button
+          text="Войти"
+          variant="primary"
+          onPress={handleSubmit(onSubmit)}
+          style={{ marginTop: 20 }}
+          // disabled={!isValid}
+        />
+        <Button
+          style={{ marginTop: 8 }}
+          variant="secondary"
+          text="Восстановить пароль"
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
